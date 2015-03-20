7267f95640a884ce7908213d6e62b5a3e1a63579
Seichis <koumou276@gmail.com>
2015-03-20 13:19:51 +0100

    fluency completed

---
+++ app/src/main/java/canvas/CustomProgressBar.java
@@ -0,0 +1,47 @@
+package canvas;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import com.performance.cognitive.datagathering.TrailMakingActivity;
+
+/**
+ * Created by User1 on 20/3/2015.
+ */
+public class CustomProgressBar {
+    private Paint barPaint;
+    private float barStartX;
+    private float barStartY;
+    private float barEndX;
+    private float barEndY;
+    private float currentSeekBarLength = 0;
+
+
+    CustomProgressBar(int sX,int sY,int eX,int eY) {
+        barPaint = new Paint();
+        barStartX = sX;
+        barStartY = sY;
+        barEndX = eX;
+        barEndY = eY;
+    }
+
+    private void setCurrentSeekBarLength(float x) {
+
+        this.currentSeekBarLength = x;
+    }
+
+    private float getCurrentSeekBarLength() {
+
+        return this.currentSeekBarLength;
+    }
+
+    public void drawProgressBar(Canvas c) {
+        barPaint.setColor(Color.WHITE);
+        setCurrentSeekBarLength((barEndX - barStartX) * (TrailMakingActivity.getSecondElapsed() / 30));
+        barPaint.setColor(Color.BLUE);
+        c.drawRect(barStartX, barStartY, getCurrentSeekBarLength(),
+                barEndY, barPaint);
+
+    }
+}
