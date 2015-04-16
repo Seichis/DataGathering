a407d2aa42f75b4aefb1417629a2755df6db1663
Seichis <koumou276@gmail.com>
2015-04-16 13:14:56 +0200

    ALL IN!!!

--- app/src/main/java/canvas/DrawingPanelOneShot.java
+++ app/src/main/java/canvas/DrawingPanelOneShot.java
@@ -175,7 +175,7 @@ public class DrawingPanelOneShot extends ImageView implements View.OnTouchListen
 
                 Bitmap b = Bitmap.createBitmap(this.getDrawingCache(false));
                 ImageAdapter.addImage(b);
-                this.setDrawingCacheEnabled(false);
+                this.destroyDrawingCache();
                 temp = pmSetList.size();
 
             }
