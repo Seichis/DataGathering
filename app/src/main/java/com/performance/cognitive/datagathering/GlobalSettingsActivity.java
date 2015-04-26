ce7698e05691059e3e960c0504788742cd019158
Seichis <koumou276@gmail.com>
2015-04-27 01:54:30 +0200

    ALL IN!!!

--- app/src/main/java/com/performance/cognitive/datagathering/GlobalSettingsActivity.java
+++ app/src/main/java/com/performance/cognitive/datagathering/GlobalSettingsActivity.java
@@ -1,13 +1,10 @@
 package com.performance.cognitive.datagathering;
 
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +13,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class GlobalSettingsActivity extends ActionBarActivity {
+public class GlobalSettingsActivity extends Activity {
     EditText inputDO, inputDS, inputCoordRad, inputCoordDist, inputLT;
     Button saveSettings, startSession;
     SharedPreferences pref;
@@ -52,7 +49,7 @@ public class GlobalSettingsActivity extends ActionBarActivity {
                 }
 
                 mHandler = new Handler();
-                mTimer.scheduleAtFixedRate(new ActionsTimerTask(),300000,3600000);
+                mTimer.scheduleAtFixedRate(new ActionsTimerTask(), 300000, 3600000);
 
             }
         });
@@ -73,28 +70,6 @@ public class GlobalSettingsActivity extends ActionBarActivity {
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_global_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     class ActionsTimerTask extends TimerTask {
 
