bc7d4b59e661937c460462ddee4a774c168ad908
Seichis <koumou276@gmail.com>
2015-04-28 17:27:34 +0200

    ALL IN!!!

---
+++ app/src/main/java/datastructure/StaticTaskTypes.java
@@ -0,0 +1,16 @@
+package datastructure;
+
+/**
+ * Created by User1 on 28/4/2015.
+ */
+public class StaticTaskTypes {
+    public static final String digitOrder = "attentiondigitorder";
+    public static final String digitSpan ="attentiondigitspan";
+    public static final String coord ="coordination";
+    public static final String fluency ="fluency";
+    public static final String longterm ="longtermmemory";
+    public static final String speedtap ="speedtap";
+    public static final String reaction ="reactiontime";
+    public static final String selective ="selectiveattention";
+    public static final String speednum ="speednumber";
+}
